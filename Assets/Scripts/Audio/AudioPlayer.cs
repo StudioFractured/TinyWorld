24992685e2e4579b79db682198156fce3aca25c9
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-05-20 19:26:00 -0300

    feat: audio system added.

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] AudioSO _so = null;
+    [SerializeField] bool _playOnStart = false;
+
+    private void Start()
+    {
+        if (_playOnStart)
+        {
+            Play();
+        }
+    }
+
+    [ContextMenu("Play()")]
+    public void Play()
+    {
+        _so.Play(transform.position);
+    }
+}
