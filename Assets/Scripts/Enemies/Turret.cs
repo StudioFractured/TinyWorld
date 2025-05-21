274ac28de55c1bd07ac5652932d890fa6cb356a5
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-05-21 12:13:09 -0300

    feat: turret enemy flip to player added.

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    [SerializeField] SideFlipper _flipper = null;
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float shootingRate = 1f; // seconds between shots
-    public bool shootRight = true;  // Set to false to shoot left
+    //public bool shootRight = true;  // Set to false to shoot left
 
     private float shootingCooldown;
 
@@ -35,7 +36,8 @@ public class Turret : MonoBehaviour
             if (bulletScript != null)
             {
                 // Shoot far to the right or left so Mathf.Sign can detect direction
-                float offset = shootRight ? 10f : -10f;
+                //float offset = shootRight ? 10f : -10f;
+                float offset = _flipper.IsFacingRight() ? 10f : -10f;
                 Vector3 target = firePoint.position + new Vector3(offset, 0f, 0f);
                 bulletScript.SetTarget(target);
             }
